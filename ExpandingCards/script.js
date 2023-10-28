ed7d96bf057f70ff65242b77781a9808419622c3
edson <edson_ctm@hotmail.com>
2023-10-28 16:23:22 -0300

    expanding cards

---
+++ ExpandingCards/script.js
@@ -0,0 +1,13 @@
+const paineis = document.querySelectorAll('.painel')
+paineis.forEach(painel => {
+    painel.addEventListener('click', () => {
+        removeActiveClasses()
+        painel.classList.add('ativo')
+    })
+})
+
+function removeActiveClasses(){
+    paineis.forEach(painel => {
+        painel.classList.remove('ativo')
+    })
+}
